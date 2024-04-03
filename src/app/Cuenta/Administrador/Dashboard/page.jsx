313fa38532f32e78f97a628aec4c2e33ec37daad
CuackcCuack <avigail.gomez12@gmail.com>
2024-04-03 14:15:34 -0600

    Login Admin (tengo miedo)

--- src/app/Cuenta/Administrador/Dashboard/page.jsx
+++ src/app/Cuenta/Administrador/Dashboard/page.jsx
@@ -1,4 +1,72 @@
-"use client";
+import React from 'react';
+import "./dash.css"
+
+async function peticion () {
+  const res = await fetch("/api/Reportes")
+  if (!res.ok) {
+    throw new Error("Error al obtener los datos");
+  }
+  const data = await res.json()
+  const mapData = data.map(item => { return item.uidUsuario;});
+  return mapData
+}
+const reportes = peticion()
+console.log(reportes)
+
+const data = [10,20,30,40, 50]
+const datas = [
+  { name: 'A', value: 10 },
+  { name: 'B', value: 20 },
+  { name: 'C', value: 30 },
+  { name: 'D', value: 40 },
+  { name: 'E', value: 50 }
+];
+
+
+function Dashboard() {
+  return (
+    <div className="container-general">
+      <div className="filtros">
+        <div id="fechas">
+    <h4>Rango Fechas</h4>
+        </div>
+        <div id="alcaldia">
+    <h4>Alcaldia</h4>
+        </div>
+        <div id="estado">
+    <h4>Estado</h4>
+        </div>
+      </div>
+
+      <div className="reportes">
+        <div id="reportes-totales">
+    <h3>REPORTES TOTALES</h3>
+        </div>
+        <div id="reportes-no-completos">No Resuleto</div>
+        <div id="reportes-en-proceso">En Proceso</div>
+        <div id="reportes-reparados">Resuelto</div>
+      </div>
+      <div className="grafica-circular">
+        <h3>ALCALDIAS CON MAS REPORTES</h3>
+        <div className="circular">
+        </div>
+      </div>
+      <div className="grafica-barras">
+      <h3>REPORTES POR ALCALDIA</h3>
+
+      <div className="barras">
+      </div>
+      </div>
+      <div className="grafica-barras-hz">
+        <h3>REPORTES SEGÚN SU ESTADO DE ATENCIÓN POR ALCALDIA</h3>
+
+      </div>
+    </div>
+  )
+}
+
+export default Dashboard
+/* "use client";
 import React, { useState, useEffect, useContext } from 'react';
 import './dash.css';
 import { signOut } from "firebase/auth";
@@ -259,4 +327,4 @@ export default function Dashboard(){
     </div>
     
   );
-}
+} */
