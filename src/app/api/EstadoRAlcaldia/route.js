13c300a4a87a270a398758fecde485d38bb25a4c
EmilianoRivera <jriveras2100@alumno.ipn.mx>
2024-04-05 17:08:20 -0600

    graficas

---
+++ src/app/api/EstadoRAlcaldia/route.js
@@ -0,0 +1,53 @@
+import { NextResponse } from "next/server";
+import { db, collection, getDocs } from "../../../../firebase";
+
+export async function GET(request) {
+  try {
+    const reportesRef = collection(db, "reportes");
+    const reportesSnapshot = await getDocs(reportesRef);
+    const datos = {};
+
+    const estadoMapping = {
+      "Sin atender": "sinAtender",
+      "En atención": "enAtencion",
+      Atendido: "atendido",
+    };
+
+    reportesSnapshot.forEach((doc) => {
+      const reporte = doc.data();
+      const ubicacion = reporte.ubicacion;
+
+      const alcaldiasEnUbicacion = obtenerAlcaldiasEnUbicacion(ubicacion);
+
+      if (alcaldiasEnUbicacion && alcaldiasEnUbicacion.length > 0) {
+        alcaldiasEnUbicacion.forEach((alcaldia) => {
+          if (!datos[alcaldia]) {
+            datos[alcaldia] = {
+              sinAtender: 0,
+              enAtencion: 0,
+              atendido: 0,
+            };
+          }
+
+          const estadoNormalizado = estadoMapping[reporte.estado];
+          if (estadoNormalizado) {
+            datos[alcaldia][estadoNormalizado]++;
+          }
+        });
+      }
+    });
+
+    console.log(datos);
+
+    return NextResponse.json(datos);
+  } catch (error) {
+    console.error("Error al obtener reportes:", error);
+    return NextResponse.error("Error al obtener reportes", { status: 500 });
+  }
+}
+function obtenerAlcaldiasEnUbicacion(ubicacion) {
+  const regexAlcaldiasCDMX =
+    /(Azcapotzalco|Coyoacán|Cuajimalpa de Morelos|Gustavo A. Madero|Iztacalco|Iztapalapa|Magdalena Contreras|Miguel Hidalgo|Milpa Alta|Tláhuac|Tlalpan|Venustiano Carranza|Xochimilco)/gi;
+
+  return ubicacion.match(regexAlcaldiasCDMX);
+}
--- src/components/Circular.jsx
+++ src/components/Circular.jsx
@@ -1,42 +1,146 @@
-"use client"
-import React, { useEffect, useRef } from 'react';
-import * as d3 from 'd3';
+import React, { useEffect, useRef, useState } from "react";
+import * as d3 from "d3";
 
-export default function Circular ({ data, width, height }) {
+export default function Circular({ width, height, estado }) {
+  //AQUI ESTAN LOS ESTADOS Y EL HOOK DE USEREF, QUE HACE REFERENCIA AL ELEMENTO SVG QUE ESTA EN EL HTML
   const svgRef = useRef();
+  const tooltipRef = useRef();
+  const [rep, setRep] = useState([]);
+  const [totalRep, setTotalRep] = useState(0);
+  const [selectedSegment, setSelectedSegment] = useState(null);
+  const [alcEstRep, setAlcEstRep] = useState();
 
+  //SE ENCARGA DE HACER LAS PETICIONES A LOS ENDPOINTS PARA TRAER LA INFORMACIÓN QUE SE VA A GRAFICAR, EN EL SVG ES DONDE SE PINTAN LAS GRAFICAS
   useEffect(() => {
-    if (!data || !data.length) return;
+    async function fetchData() {
+      try {
+        const response = await fetch("/api/g1"); //G1 ES GRAFICA 1 , OSEA LA CIRCULAR
+        const totalRep = await fetch("/api/reportesTotales"); // TRAE EL NUMERO DE REPORTES TOTALES QUE SE HICIERON
+        const estadoReporteAlcaldia = await fetch("/api/EstadoRAlcaldia"); //TRAE POR ALCALDIA EL NUMERO DE REPORTES QUE ESTA EN ESTADO: SIN ATENDER, EN ATENCION Y ATENDIDO
+        if (!response.ok || !totalRep.ok || !estadoReporteAlcaldia.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        const data = await response.json();
+        const data2 = await totalRep.json();
+        const data3 = await estadoReporteAlcaldia.json();
+        // Convertir el objeto en un array de objetos
+        const dataArray = Object.entries(data).map(([label, value]) => ({
+          label,
+          value,
+        }));
 
-    const svg = d3.select(svgRef.current);
-    const radius = Math.min(width, height) / 2;
-    const color = d3.scaleOrdinal(d3.schemeCategory10);
+        setRep(dataArray);
+        setTotalRep(data2);
+        setAlcEstRep(data3);
+        console.log(data3);
+      } catch (error) {
+        console.log("Error fetching data: ", error);
+      }
+    }
 
-    const pie = d3.pie().value(d => d);
+    fetchData();
+  }, []);
+  //FUNCION QUE SE ENCARGA DE ACTUALIZAR LA GRAFICA CON BASE AL ESTADO
+  function nuevaGraficaCircular(alcEstRep) {
 
-    const arc = d3.arc()
-      .innerRadius(0)
-      .outerRadius(radius);
+    
+  }
+  //HOOK QUE SE ENCARGA DE CREAR LA GRAFICA CON BASE AL PORCETAJE QUE HAY DE REPORTES POR ALCALDIA, ESTA APARECE PRIMERO, SI HAY UN CAMBIA DE ESTADO YA APARECE LA GRAFICA DE LA FUNCION nuevaGraficaCircular
+  useEffect(() => {
+    if (!rep || !rep.length) return;
+    else if (estado === "sin estado") {
+      const svg = d3.select(svgRef.current);
+      const radius = Math.min(width, height) / 2;
+      const color = d3.scaleOrdinal(d3.schemeCategory10);
 
-    const arcs = svg.selectAll("arc")
-      .data(pie(data))
-      .enter()
-      .append("g")
-      .attr("class", "arc")
-      .attr("transform", `translate(${width / 2}, ${height / 2})`);
+      const pie = d3.pie().value((d) => d.value);
 
-    arcs.append("path")
-      .attr("fill", (d, i) => color(i))
-      .attr("d", arc);
+      const arc = d3.arc().innerRadius(50).outerRadius(radius);
 
-    arcs.append("text")
-      .attr("transform", d => `translate(${arc.centroid(d)})`)
-      .attr("text-anchor", "middle")
-      .text(d => d.data);
+      const arcs = svg
+        .selectAll("arc")
+        .data(pie(rep))
+        .enter()
+        .append("g")
+        .attr("class", "arc")
+        .attr("transform", `translate(${width / 2}, ${height / 2})`);
 
-  }, [data, height, width]);
+      arcs
+        .append("path")
+        .attr("fill", (d, i) => color(i))
+        .attr("d", arc)
+        .on("mouseover", (event, d) => {
+          setSelectedSegment(d);
+        })
+        .on("mouseout", () => {
+          setSelectedSegment(null);
+        });
 
-  return <svg ref={svgRef} width={width} height={height}></svg>;
+      arcs
+        .append("text")
+        .attr("transform", (d) => `translate(${arc.centroid(d)})`)
+        .attr("text-anchor", "middle")
+        .style("font-family", "Helvetica, sans-serif")
+        .text((d) => d.data.label.toUpperCase());
+
+      // Agregar el porcentaje fijo debajo de cada alcaldía
+      arcs
+        .append("text")
+        .attr("transform", (d) => {
+          const centroid = arc.centroid(d);
+          const x = centroid[0];
+          const y = centroid[1] + 20; // Ajusta la posición vertical del porcentaje fijo
+          return `translate(${x}, ${y})`;
+        })
+        .attr("text-anchor", "middle")
+        .attr("dy", "1em") // Ajusta la distancia vertical del texto
+        .text(
+          (d) =>
+            `${(((d.endAngle - d.startAngle) / (2 * Math.PI)) * 100).toFixed(
+              2
+            )}%`
+        );
+
+      const tooltip = d3.select(tooltipRef.current);
+      tooltip.style("visibility", "hidden");
+    }
+  }, [rep, height, width]);
+
+  //ESTE USEEFFECT SE ENCARGA DE OCULTAR LOS ELEMENTOS, Y REVISAR QUE SI CAMBIA ALGO EN EL FILTRO DEL ESTADO, SE EJECUTE LA FUNCION QUE CAMBIA LA GRAFICA
+  useEffect(() => {
+    if (!selectedSegment) {
+      d3.select(tooltipRef.current).style("visibility", "hidden");
+    } else if (estado === "sin estado") {
+      d3.select(tooltipRef.current).style("visibility", "visible");
+      d3.select(tooltipRef.current)
+        .select(".tooltip-label")
+        .style("font-family", "Helvetica, sans-serif")
+        .text(selectedSegment.data.label.toUpperCase());
+      const percentage = (
+        ((selectedSegment.endAngle - selectedSegment.startAngle) /
+          (2 * Math.PI)) *
+        100
+      ).toFixed(2);
+      d3.select(tooltipRef.current)
+        .select(".tooltip-value")
+        .text(`${percentage}%`);
+    } else {
+      console.log("NUEVA DATAA", alcEstRep);
+      nuevaGraficaCircular(alcEstRep);
+    }
+  }, [selectedSegment]);
+
+  return (
+    <div style={{ position: "relative", width, height }}>
+      <svg ref={svgRef} width={width} height={height}></svg>
+      <div
+        ref={tooltipRef}
+        className="tooltip"
+        style={{ position: "absolute", top: 10, right: 10 }}
+      >
+        <div className="tooltip-label"></div>
+        <div className="tooltip-value"></div>
+      </div>
+    </div>
+  );
 }
-
- 
