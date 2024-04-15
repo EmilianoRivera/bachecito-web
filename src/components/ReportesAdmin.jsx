69ea98ff24c4f4e8975a844c7e1f581929002c67
EmilianoRivera <jriveras2100@alumno.ipn.mx>
2024-04-15 00:04:57 -0600

    eliminar reporte admin

--- src/components/ReportesAdmin.jsx
+++ src/components/ReportesAdmin.jsx
@@ -1,10 +1,15 @@
 
 "use client"
 import { useEffect, useState } from "react";
+import { db, collection, getDocs, deleteDoc, doc } from "../../firebase";
+
 import '../app/Cuenta/Administrador/Reportes/Reportes.css';
 import React from 'react';
 
 export default function ReportesAdmin() {
+
+
+    
     const [rep, setRep] = useState([]);
     function showDeleteHeader() {
         const table = document.querySelector('.containerReportesAdmin table');
@@ -34,6 +39,32 @@ export default function ReportesAdmin() {
         fetchData();
     }, []);
 
+    const handleClick = async (folio) =>  {
+        try {
+            const refCollection = collection(db, 'reportes');
+            const reportesSnapshot = await getDocs(refCollection);
+            
+            reportesSnapshot.forEach(async (reporteDoc) => {
+                const reporte = reporteDoc.data();
+                if (reporte.folio === folio) {
+                    await deleteDoc(doc(refCollection, reporteDoc.id));
+                    console.log(`Se eliminó el reporte con folio ${folio}`);
+                    
+                    // Eliminar la fila de la tabla HTML
+                    const rows = document.querySelectorAll('.containerReportesAdmin .Reportes');
+                    rows.forEach((row) => {
+                        if (row.querySelector('.folio').textContent === folio) {
+                            row.remove();
+                        }
+                    });
+                }
+            });
+        } catch (error) {
+            console.error("Error al obtener los reportes", error);
+        }
+    };
+
+     
     return (
         <div className="containerReportesAdmin">
             <table>
@@ -58,7 +89,7 @@ export default function ReportesAdmin() {
                             <td className='ubicacion'>{report.ubicacion}</td>
                             <td className='no-reportes'>-</td>
                             <td className='eliminar'>
-                                <button className="btn-eliminarRP">
+                                <button className="btn-eliminarRP" onClick={() => {handleClick(report.folio)}}>
                                     <img src="https://i.postimg.cc/02gZVXL3/basura.png" alt="" />
                                 </button>
                             </td>
