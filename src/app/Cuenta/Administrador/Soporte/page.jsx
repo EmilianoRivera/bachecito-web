f7e10e8589a93cb03e282e4d90b7209d99ef25df
EmilianoRivera <jriveras2100@alumno.ipn.mx>
2024-04-30 12:40:25 -0600

    trato de jalar cambios de axel

--- src/app/Cuenta/Administrador/Soporte/page.jsx
+++ src/app/Cuenta/Administrador/Soporte/page.jsx
@@ -1,51 +1,72 @@
 "use client";
-import React, { useState } from 'react';
-import RutaProtegida from "@/components/RutaProtegida";
-import "./Soporte.css";
-
+import React, { useState, useEffect, useContext } from "react";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { useAuthUser } from "../../../../../hooks/UseAuthUser";
+import { app, auth, db } from "../../../../../firebase";
+import { useRouter } from "next/navigation";
+import AuthContext from "../../../../../context/AuthContext";
+import "./Reportes.css";
 
 function Soporte() {
+  useAuthUser();
+  const router = useRouter();
+  const { isLogged } = useContext(AuthContext);
+  const [userData, setUserData] = useState({});
+  const [errorSeleccionado, setErrorSeleccionado] = useState("S001");
+  const [sistemaOperativo, setSistemaOperativo] = useState(
+    "No se ha seleccionado un sistema operativo"
+  );
+  const [navegador, setNavegador] = useState(
+    "No se ha seleccionado un navegador"
+  );
+  const [selectedRutaError, setSelectedRutaError] = useState("/NoEspecificado");
+  const [foto, setFoto] = useState("");
+  const [descripcionProblema, setDescripcionProblema] =
+    useState("Sin descripcion");
 
-  const [mostrarDetalle1, setMostrarDetalle1] = useState(false);
-  const [mostrarDetalle2, setMostrarDetalle2] = useState(false);
-  const [mostrarDetalle3, setMostrarDetalle3] = useState(false);
-  const [mostrarDetalle4, setMostrarDetalle4] = useState(false);
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const unsubscribe = auth.onAuthStateChanged((user) => {
+        if (user) {
+          const uid = user.uid;
+          fetchData(uid);
+        } else {
+          router.push("/reportes");
+        }
+      });
+      return () => unsubscribe();
+    }
 
-  const toggleDetalle1 = () => {
-    setMostrarDetalle1(!mostrarDetalle1);
-  };
+    async function fetchData(uid) {
+      try {
+        const userResponse = await fetch(`/api/Usuario/${uid}`);
+        if (!userResponse.ok) {
+          throw new Error("Failed to fetch user data");
+        }
+        const userDatas = await userResponse.json();
 
-  const toggleDetalle2 = () => {
-    setMostrarDetalle2(!mostrarDetalle2);
-  };
-
-  const toggleDetalle3 = () => {
-    setMostrarDetalle3(!mostrarDetalle3);
-  };
-
-  const toggleDetalle4 = () => {
-    setMostrarDetalle4(!mostrarDetalle4);
-  };
-
-  // Función para cambiar la imagen
-  const obtenerImagen1 = () => {
-    return mostrarDetalle1 ? 'https://i.postimg.cc/wB2S0JqV/triangulo-1.png' : 'https://i.postimg.cc/Z5XxTn6Y/triangulo.png';
-  };
-
-  const obtenerImagen2 = () => {
-    return mostrarDetalle2 ? 'https://i.postimg.cc/wB2S0JqV/triangulo-1.png' : 'https://i.postimg.cc/Z5XxTn6Y/triangulo.png';
-  };
-
-  const obtenerImagen3 = () => {
-    return mostrarDetalle3 ? 'https://i.postimg.cc/wB2S0JqV/triangulo-1.png' : 'https://i.postimg.cc/Z5XxTn6Y/triangulo.png';
-  };
-
-  const obtenerImagen4 = () => {
-    return mostrarDetalle4 ? 'https://i.postimg.cc/wB2S0JqV/triangulo-1.png' : 'https://i.postimg.cc/Z5XxTn6Y/triangulo.png';
-  };
+        setUserData(userDatas);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+  }, []);
+  const catalogoRutaErrores = [
+    { ruta: "/Cuenta/Administrador", modulo: "Inicio de Sesión" },
+    { ruta: "/Administrador/Dashboard", modulo: "Dashboard" },
+    { ruta: "/Administrador/Mapa", modulo: "Mapa" },
+    { ruta: "/Administrador/NuevoAdmin", modulo: "Nuevo Administrador" },
+    { ruta: "/Administrador/Reportes", modulo: "Reportes" },
+    { ruta: "/Administrador/Papelera", modulo: "Reportes" },
+    { ruta: "Otros", modulo: "Otra opción" },
+  ];
+/*
+Inicio de sesion , Nuevo Administrador, Otros -> ALTA
+DASH, MAPA, REPORTES, PAPELERA -> Media
 
 
 
+*/
   // Catálogo de errores
   const catalogoErrores = [
     { clave: "S001", nombre: "Error de Inicio de Sesión" },
@@ -59,7 +80,7 @@ function Soporte() {
     { clave: "R003", nombre: "Error al Mover reportes a la papelera" },
     { clave: "P001", nombre: "Error al Visualizar reportes en la papelera" },
     { clave: "P002", nombre: "Error al Eliminar reportes de la papelera" },
-    { clave: "S001", nombre: "Error al Enviar Ticket" },
+    { clave: "T001", nombre: "Error al Enviar Ticket" },
     { clave: "0000", nombre: "Otro: (Especificar en Descripcion)" },
   ];
 
@@ -87,36 +108,8 @@ function Soporte() {
     "Opera",
     "Internet Explorer",
     "Samsung Internet",
-    "Otro"
+    "Otro",
   ];
-
-
-
-  const [fecha, setFecha] = useState('');
-  const [errorSeleccionado, setErrorSeleccionado] = useState('No se ha seleccionado un error');
-  const [sistemaOperativo, setSistemaOperativo] = useState('No se ha seleccionado un sistema operativo');
-  const [navegador, setNavegador] = useState('No se ha seleccionado un navegador');
-  const [rutaError, setRutaError] = useState('');
-  const [foto, setFoto] = useState('');
-  const [descripcionProblema, setDescripcionProblema] = useState('');
-
-  // Obtener fecha actual al cargar el componente
-  /*
-  useEffect(() => {
-    const obtenerFechaActual = () => {
-      const fechaActual = new Date();
-      const dia = fechaActual.getDate();
-      const mes = fechaActual.getMonth() + 1;
-      const año = fechaActual.getFullYear();
-      const fechaFormateada = `${dia < 10 ? '0' + dia : dia}/${mes < 10 ? '0' + mes : mes}/${año}`;
-      setFecha(fechaFormateada);
-    };
-
-    obtenerFechaActual();
-  }, []);
-  */
-
-  // Funciones para manejar los cambios en el select de errores y sistemas operativos
 
   const handleError = (e) => {
     const selectedErr = e.target.value;
@@ -128,14 +121,10 @@ function Soporte() {
     const selectedSO = e.target.value;
     setSistemaOperativo(selectedSO);
     console.log(selectedSO);
-    setSistemaOperativo(selectedSO);
-    console.log(selectedSO);
   };
 
   const handleNavegador = (e) => {
     const selectedNavegador = e.target.value;
-    setNavegador(selectedNavegador);
-    console.log(selectedNavegador);
     setNavegador(selectedNavegador);
     console.log(selectedNavegador);
   };
@@ -178,20 +167,51 @@ function Soporte() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    const fechaTicket = new Date();
+    const correo = userData.correo;
+    const nombre = userData.nombre;
+    const url = await handleFileUpload();
+    console.log(
+      errorSeleccionado,
+      " ",
+      sistemaOperativo,
+      " ",
+      navegador,
+      " ",
+      selectedRutaError,
+      " ",
+      descripcionProblema,
+      " ",
+      url,
+      " ",
+      correo,
+      " ",
+      nombre,
+      " "
+    );
+    const parametros = {
+      errorSeleccionado: errorSeleccionado,
+      sistemaOperativo: sistemaOperativo,
+      navegador: navegador,
+      selectedRutaError: encodeURIComponent(selectedRutaError),
+      descripcionProblema: descripcionProblema,
+      fechaTicket: fechaTicket,
+      correo: correo,
+      nombre: nombre,
+    };
     try {
-      const response = await fetch('/api/Soporte', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          errorSeleccionado,
-          sistemaOperativo,
-          navegador,
-          rutaError,
-          descripcionProblema
-        })
-      });
+      const response = await fetch(
+        `/api/Soporte/${errorSeleccionado}/${sistemaOperativo}/${navegador}/${encodeURIComponent(
+          selectedRutaError
+        )}/${descripcionProblema}/${fechaTicket}/${correo}/${nombre}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(parametros),
+        }
+      );
 
       if (response.ok) {
         console.log("Formulario enviado con éxito");
@@ -203,189 +223,95 @@ function Soporte() {
     }
   };
 
-  // Catálogo de errores, SO, versión de navegador, descripción del problema, ruta donde se encontró el error, evidencia fotográfica del problema, 
-  // Internamente se obtiene fecha, prioridad con base al error seleccionado, número del ticket, su correo
-
-  //<input type="text" value={fecha} readOnly />
-
   return (
-
-    <RutaProtegida>
-      <div className="bodySoporte">
-        <div className="containerSoporte">
-          <div className='containerPF'>
-            <h1 id='titulo_pf'>PREGUNTAS FRECUENTES 👀❓❓❓</h1>
-
-            <div className='container_preguntaFrecuente'>
-              <div className='pf'>
-                <p>1.- ¿Cómo instalo la app móvil de Bachecito 26?</p>
-                <img src={obtenerImagen1()} alt="" onClick={toggleDetalle1} />
-              </div>
-              {mostrarDetalle1 && (
-                <div className="descripcion_pf">
-                  <p>
-                    <br />
-                    Detalle de la respuesta a la pregunta frecuente 1.
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum aspernatur inventore optio cumque eius. Quisquam facilis quo possimus omnis veniam, provident odit architecto dolore, minima, placeat maiores alias sed recusandae.
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti suscipit labore quo molestiae quis illo sunt nulla cupiditate magni voluptatem eos aliquam impedit mollitia officia, minus distinctio ullam voluptates earum?
-                    <br />
-                  </p>
-                </div>
-              )}
-            </div>
-
-            <br />
-
-            <div className='container_preguntaFrecuente'>
-              <div className='pf'>
-                <p>2.- ¿Cómo desinstalo la app móvil de Bachecito 26?</p>
-                <img src={obtenerImagen2()} alt="" onClick={toggleDetalle2} />
-              </div>
-              {mostrarDetalle2 && (
-                <div className="descripcion_pf">
-                  <p>
-                    <br />
-                    Detalle de la respuesta a la pregunta frecuente 2.
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum aspernatur inventore optio cumque eius. Quisquam facilis quo possimus omnis veniam, provident odit architecto dolore, minima, placeat maiores alias sed recusandae.
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti suscipit labore quo molestiae quis illo sunt nulla cupiditate magni voluptatem eos aliquam impedit mollitia officia, minus distinctio ullam voluptates earum?
-                    <br />
-                  </p>
-                </div>
-              )}
-            </div>
-
-            <br />
-
-            <div className='container_preguntaFrecuente'>
-              <div className='pf'>
-                <p>2.- ¿Cómo desinstalo la app móvil de Bachecito 26?</p>
-                <img src={obtenerImagen3()} alt="" onClick={toggleDetalle3} />
-              </div>
-              {mostrarDetalle3 && (
-                <div className="descripcion_pf">
-                  <p>
-                    <br />
-                    Detalle de la respuesta a la pregunta frecuente 2.
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum aspernatur inventore optio cumque eius. Quisquam facilis quo possimus omnis veniam, provident odit architecto dolore, minima, placeat maiores alias sed recusandae.
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti suscipit labore quo molestiae quis illo sunt nulla cupiditate magni voluptatem eos aliquam impedit mollitia officia, minus distinctio ullam voluptates earum?
-                    <br />
-                  </p>
-                </div>
-              )}
-            </div>
-
-            <br />
-
-            <div className='container_preguntaFrecuente'>
-              <div className='pf'>
-                <p>2.- ¿Cómo desinstalo la app móvil de Bachecito 26?</p>
-                <img src={obtenerImagen4()} alt="" onClick={toggleDetalle4} />
-              </div>
-              {mostrarDetalle4 && (
-                <div className="descripcion_pf">
-                  <p>
-                    <br />
-                    Detalle de la respuesta a la pregunta frecuente 2.
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum aspernatur inventore optio cumque eius. Quisquam facilis quo possimus omnis veniam, provident odit architecto dolore, minima, placeat maiores alias sed recusandae.
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti suscipit labore quo molestiae quis illo sunt nulla cupiditate magni voluptatem eos aliquam impedit mollitia officia, minus distinctio ullam voluptates earum?
-                    <br />
-                  </p>
-                </div>
-              )}
-            </div>
-          </div>
-
-          <div className='container_FormularioSoporte'>
-            <div className='containerFR'>
-              <br /><br /><br /><br />
-              <h2>Hola este es un formulario para el soporte :D</h2>
-
-              <form onSubmit={handleSubmit}>
-
-                {/* 
-                <label>Fecha:</label>
-                <input type="text" defaultValue={fecha} readOnly/>
-                <br /><br />
-
-                <h5>Obtener nombre del usuario acá</h5>
-                <br />
-
-                <h5>Obtener correo del usuario acá</h5>
-                <br />
-              */}
-
-                <label>Seleccione el error:</label>
-                <select value={errorSeleccionado} onChange={handleError}>
-                  <option value="">Seleccionar</option>
-                  {catalogoErrores.map((error, index) => (
-                    <option key={index} value={error.nombre}>
-                      {`${error.nombre}`}
-                    </option>
-                  ))}
-                </select>
-                <br /><br /><br />
-
-                <label>Seleccione su sistema operativo: </label>
-                <select value={sistemaOperativo} onChange={handleSO}>
-                  <option value="">Seleccionar</option>
-                  {catalogoSistemaOperativo.map((sistema, index) => (
-                    <option key={index} value={sistema}>
-                      {`${sistema}`}
-                    </option>
-                  ))}
-                </select>
-                <br /><br /><br />
-
-                <label>Seleccione su navegador: </label>
-                <select
-                  value={navegador}
-                  onChange={handleNavegador}
-                >
-                  <option value="">Seleccionar</option>
-                  {catalogoNavegadores.map((navegador, index) => (
-                    <option key={index} value={navegador}>
-                      {`${navegador}`}
-                    </option>
-                  ))}
-                </select>
-                <br /><br /><br />
-
-                <label>Ruta donde se encontró el error: </label>
-                <input
-                  type="text"
-                  value={rutaError}
-                  onChange={handleRutaError}
-                />
-                <br /><br /><br />
-
-                <label>Adjuntar fotografía del problema: </label>
-                <input
-                  type="file"
-                  accept="image/*"
-                  onChange={handleFoto}
-                />
-                <br /><br /><br />
-
-                <label>Descripción del problema: </label>
-                <textarea
-                  value={descripcionProblema}
-                  onChange={handleDescripcionProblema}
-                  rows="4"
-                  cols="50"
-                />
-                <br /><br /><br />
-
-                <button type="submit" id="submit">Enviar</button>
-
-              </form>
-            </div>
-          </div>
+    <div className="bodySoporte">
+      <div className="containerSoporte">
+        <div className="containerPF">
+          <h1>PREGUNTAS FRECUENTES ❓❓❓</h1>
         </div>
       </div>
-    </RutaProtegida>
+      <div className="main-containerReportes">
+        <br />
+        <br />
+        <br />
+        <br />
+        <h2>Hola este es un formulario para el soporte :D</h2>
 
+        <form onSubmit={handleSubmit}>
+          <label>Seleccione el error:</label>
+          <select value={errorSeleccionado} onChange={handleError}>
+            <option>Tipo de Error</option>
+            {catalogoErrores.map((errorSeleccionado, index) => (
+              <option key={index} value={errorSeleccionado.clave}>
+                {`${errorSeleccionado.nombre}`}
+              </option>
+            ))}
+          </select>
+          <br />
+          <br />
+          <br />
+          <label>Módulo donde se encontró el error: </label>
+          <select value={selectedRutaError} onChange={handleRutaError}>
+            <option>Módulo del Error</option>
+            {catalogoRutaErrores.map((errorOption, index) => (
+              <option key={index} value={errorOption.ruta}>
+                {`${errorOption.modulo}`}
+              </option>
+            ))}
+          </select>
+          <br />
+          <br />
+          <br />
+          <label>Seleccione su sistema operativo: </label>
+          <select value={sistemaOperativo} onChange={handleSO}>
+            <option value="">Seleccionar</option>
+            {catalogoSistemaOperativo.map((sistema, index) => (
+              <option key={index} value={sistema}>
+                {`${sistema}`}
+              </option>
+            ))}
+          </select>
+          <br />
+          <br />
+          <br />
+
+          <label>Seleccione su navegador: </label>
+          <select value={navegador} onChange={handleNavegador}>
+            <option value="">Seleccionar</option>
+            {catalogoNavegadores.map((navegador, index) => (
+              <option key={index} value={navegador}>
+                {`${navegador}`}
+              </option>
+            ))}
+          </select>
+          <br />
+          <br />
+          <br />
+
+          <label>Adjuntar fotografía del problema: </label>
+          <input type="file" accept="image/*" onChange={handleFoto} />
+          <br />
+          <br />
+          <br />
+
+          <label>Descripción del problema: </label>
+          <textarea
+            value={descripcionProblema}
+            onChange={handleDescripcionProblema}
+            rows="4"
+            cols="50"
+          />
+          <br />
+          <br />
+          <br />
+
+          <button type="submit" id="submit">
+            Enviar
+          </button>
+        </form>
+      </div>
+    </div>
   );
 }
 
 export default Soporte;
-
