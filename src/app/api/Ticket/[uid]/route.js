5d91ea5abbad6676bc6b2946b907bbe9004482d9
EmilianoRivera <jriveras2100@alumno.ipn.mx>
2024-05-10 18:41:01 -0600

    cambios en la ruta de ticket de soporte

--- src/app/api/Ticket/[uid]/route.js
+++ src/app/api/Ticket/[uid]/route.js
@@ -1,5 +1,31 @@
 import { NextResponse } from "next/server";
-import { db, collection, getDocs, query, where} from "../../../../../firebase";
+import { db2, collection, getDocs, query, where} from "../../../../../firebase";
+
+export async function GET(request, {params}) {
+  try {
+    const uid = params.uid
+
+    const ticketsRef = collection(db2, 'tickets')
+    const q = query(ticketsRef,  where("uid", "==", uid));
+    const ticketsSnapshot = await getDocs(q);
+
+    const tickets = [];
+
+    ticketsSnapshot.forEach((doc) => {
+      const ticket = doc.data();
+      tickets.push(ticket);
+    });
+
+    return NextResponse.json(tickets);
+  } catch (error) {
+    console.error("Error al obtener los tickets:", error);
+    return NextResponse.error("Error al obtener tickets", { status: 500 });
+  }
+}
+
+
+/* import { NextResponse } from "next/server";
+import { db2, collection, getDocs, query, where} from "../../../../../firebase";
 
 export async function GET(request, {params}) {
   try {
@@ -15,4 +41,4 @@ export async function GET(request, {params}) {
     console.error("Error al obtener los tickets:", error);
     return NextResponse.error("Error al obtener tickets", { status: 500 });
   }
-}
+} */
