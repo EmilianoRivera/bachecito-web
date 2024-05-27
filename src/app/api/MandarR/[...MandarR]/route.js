6d6aea0d55dfe1795daa7f6cfee3e2e64a86e983
EmilianoRivera <jriveras2100@alumno.ipn.mx>
2024-05-27 01:28:45 -0600

    ya sube el reporte a la bd, pero falta, automatizar el contador, y el folio

--- src/app/api/MandarR/[...MandarR]/route.js
+++ src/app/api/MandarR/[...MandarR]/route.js
@@ -4,53 +4,27 @@ export async function POST(request, { params }) {
   try {
     const [uidUsuario, nombre, apellidoPaterno, url, descripcion, ubicacion] =
       params.MandarR;
-    console.log(
-      uidUsuario,
-      " ",
-      nombre,
-      " ",
-      apellidoPaterno,
-      " ",
-      imagenURL,
-      " ",
-      ubicacion,
-      " ",
-      descripcion
-    );
-    const imagenURL = decodeURIComponent(url);
-
-    const folio = "002-5";
-
+      const imagenURL = decodeURIComponent(url);
+ /*
+ AUTOMATIZAR FOLIO, FECHA, Y CONTADOR */
+    const folio = "002-6";
+    const contador = 4
+    const fechaReporte  = "27/5/2024"
     const docRef = await addDoc(collection(db, "reportes"), {
       apellidoPaterno,
-      contador: 5,
+      contador: contador,
       descripcion,
       eliminado: false,
       estado: "Sin atender",
-      fechaReporte: "22/5/2024",
+      fechaReporte: fechaReporte,
       folio,
       imagenURL,
       nombre,
       ubicacion,
       uidUsuario,
     });
-
-    /*
-
-apellidoPaterno
-contador,
-descripcion
-eliminado
-estado
-fechaReporte
-folio
-imagenURL
-nombre
-ubicacion
-uidUsuario
-
-*/
-    return NextResponse.json(docRef);
+  
+    return NextResponse.json("ESTATUS 200");
   } catch (error) {
     console.error("Hubo un error en la api");
     return NextResponse.error(new Error("Hubo un error en la api"));
