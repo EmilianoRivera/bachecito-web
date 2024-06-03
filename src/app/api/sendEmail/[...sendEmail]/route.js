2b32c1d913a948f8db2f3c33a0b5bc97eacc37e0
EmilianoRivera <jriveras2100@alumno.ipn.mx>
2024-06-03 03:58:07 -0600

    oculto credenciasles ljejejeje

--- src/app/api/sendEmail/[...sendEmail]/route.js
+++ src/app/api/sendEmail/[...sendEmail]/route.js
@@ -10,8 +10,8 @@ export async function POST(req, {params}) {
     port: 465,
     secure: true,
     auth: {
-      user: "somos.gemma.01@gmail.com", // Tu correo
-      pass: "zuhxzlvmsmpxvaib", // Tu contraseña
+      user: process.env.NEXT_PUBLIC_EMAIL_USER, // Tu correo
+      pass:process.env.NEXT_PUBLIC_EMAIL_PASS, // Tu contraseña
     },
     tls: {
       rejectUnauthorized: false,
@@ -19,7 +19,7 @@ export async function POST(req, {params}) {
   });
 console.log("correo a enviar: ",to, " ", subject, " ", text)
   const mailOptions = {
-    from: "somos.gemma.01@gmail.com",
+    from: process.env.NEXT_PUBLIC_EMAIL_USER,
     to: to,
     subject: subject,
     text: text,
